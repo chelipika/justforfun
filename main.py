6d23ee6d7b5f216f6fb12e2af96a6b1dded0017d
chelipika <103322024+chelipika@users.noreply.github.com>
2024-12-31 23:41:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ fireworks = [
 ]
 def display_firework(color_code):
     for frame in fireworks:
-       
         print(f"\033[{color_code}m{frame}")
         time.sleep(0.1)
 def display_message():
-    os.system("cls")
     print(f"\033[{random.randint(30, 38)}mAlhamdulillah, I wish you protection from laziness and poorness, and everything that is good for you!")
     time.sleep(0.5)
 while True:
