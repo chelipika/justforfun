a0ab1eb5a8491482dd61bf3d8e41ccb246d62c93
chelipika <103322024+chelipika@users.noreply.github.com>
2024-12-31 22:34:45 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import time, os, random
+
+while True:
+    os.system("cls")
+    print(f"\033[{random.randint(30,38)}mAlhamdulillah, I wish you protection from laziness and poorness, and everything that is good for you")
+    time.sleep(0.5)
